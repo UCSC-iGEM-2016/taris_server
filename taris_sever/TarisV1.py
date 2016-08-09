3b1b1e9b139db01da122c927e453bc0ca152b687
apblair <apblair@ucsc.edu>
2016-08-09 16:20:14 -0700

    Add files via upload
    Database access methods were reformatted and added to setupDB.py. Methods are then imported into TarisV1.py.
    #Working on: creating methods in setupDB.py for adding data to the database.

--- taris_sever/TarisV1.py
+++ taris_sever/TarisV1.py
@@ -31,48 +31,43 @@
 
 from flask import Flask, render_template, request, jsonify
 import json
-import pickle
 import time
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from setupDB import bioDec, changeLog, Base, logBase
-
+from setupDB import bioDec, changeLog, Base, logBase, makeBioreactorEngine, makeChangeEngine, getProtocol, getValues
 
 app = Flask(__name__)
 
+
 class Taris_SW:
-
-
-    def __init__ (self):
+    def __init__(self):
         pass
 
     @app.route('/', methods=['GET'])
     def homePage():
-      '''
-      GETS and POSTS the main homepage of the website along with the data info
-      :return: index.html with passed currentSetPH
-      '''
-      print('Going to homePage (index.html)')
-      try:
-          # Get the last data that was changed by user
-          lastData = Taris_SW.getProtocol()
-          print('got lastData')
-          currentSetPH = lastData.setPH
-          currentSetTemp = lastData.setTemp
-          print('last data ph and temp grabbed')
-      except:
-          print('Could not query data in /  (home)')
-          currentSetPH = 7
-          currentSetTemp = 50
-          print('setting the ph to 7 and temp to 50 because no database')
-          pass
+        '''
+        GETS and POSTS the main homepage of the website along with the data info
+        :return: index.html with passed currentSetPH
+        '''
+        print('Going to homePage (index.html)')
+        try:
+            # Get the last data that was changed by user
+            lastData = getProtocol()
+            print('got lastData')
+            currentSetPH = lastData.setPH
+            currentSetTemp = lastData.setTemp
+            print('last data ph and temp grabbed')
+        except:
+            print('Could not query data in /  (home)')
+            currentSetPH = 7
+            currentSetTemp = 50
+            print('setting the ph to 7 and temp to 50 because no database')
+            pass
 
-      print('rendering index.html')
-      return render_template('index.html', ph=currentSetPH, temp=currentSetTemp)
+        print('rendering index.html')
+        return render_template('index.html', ph=currentSetPH, temp=currentSetTemp)
 
     #############################################################################################
-    #This part of the program was inspired by Simba.
+    # This part of the program was inspired by Simba.
     # /receive and /send aim to post data to the dictionary we have
     #
     # This is a test to see if we can make code the user enters save into the global dictionary/
@@ -84,49 +79,50 @@ class Taris_SW:
 
     @app.route('/setProtocol', methods=['POST'])
     def setProtocol():
-      '''
-      setProtocol is the POST method of the paramas page.
-      if the password is correct then the data will be committed to the changeLogDB.
-      :return: A string 'success' is returned as a check that the data was processed to the db
-      '''
-      # Process a JSON. Put JSON data in a database.
-      try:
-        # try the passcode
-        if request.form.get('pass') == 'pavlesucks':  # Check to see if the user knows the password.
-          # If the user is validated by the password then
-          setPH = request.form.get('pH')
-          setTemp = request.form.get('temp')
-          timeHold = request.form.get('timeHoldFor')
-          #print('ph, temp, and hold time were got')
-          ######### Put things into the DB ###############--#
-          mytime = time.strftime('%D %H:%M:%S')  # get server time in String form
-          #print('server time was got:' + mytime)
-          user = 'colintest'
-          passcode = 'KingPickler'
-          # Create changeLog.db object to be added
-          new_data = changeLog(timeLog=mytime, username=user, password=passcode, setPH=setPH, setTemp=setTemp, timeHold=timeHold)
-          #print('new data made for changeLog.db')
-          session = Taris_SW.makeChangeEngine()  # Get a session from the method that makes sessions
-          #print('Making session to connect to changeLog.db')
-          session.add(new_data)  # Add data to changeLog database
-          #print('data added to changelog db, please commit')
-          # session.add(new_data)
-          try:
-            session.commit()
-            #print('Recieved data added to changeLog.db.')
-            currentUser = 'Colin'
-            print('Set values/protocol changed by: ' + currentUser)
-          except:
-            print('Error in committing data.')
-        else:
-          # The password is false
-          print('The password was incorrect')
-          # Play a gif that is about getting passwords wrong
-      except:
-        print('Data was not added because an error was thrown in /setProtocol')
-      print('I finished currentRecieve')  # The server mangager now knows that this method has finished.
-      return 'success'
-    
+        '''
+        setProtocol is the POST method of the paramas page.
+        if the password is correct then the data will be committed to the changeLogDB.
+        :return: A string 'success' is returned as a check that the data was processed to the db
+        '''
+        # Process a JSON. Put JSON data in a database.
+        try:
+            # try the passcode
+            if request.form.get('pass') == 'pavlesucks':  # Check to see if the user knows the password.
+                # If the user is validated by the password then
+                setPH = request.form.get('pH')
+                setTemp = request.form.get('temp')
+                timeHold = request.form.get('timeHoldFor')
+                # print('ph, temp, and hold time were got')
+                ######### Put things into the DB ###############--#
+                mytime = time.strftime('%D %H:%M:%S')  # get server time in String form
+                # print('server time was got:' + mytime)
+                user = 'colintest'
+                passcode = 'KingPickler'
+                # Create changeLog.db object to be added
+                new_data = changeLog(timeLog=mytime, username=user, password=passcode, setPH=setPH, setTemp=setTemp,
+                                     timeHold=timeHold)
+                # print('new data made for changeLog.db')
+                session = makeChangeEngine()  # Get a session from the method that makes sessions
+                # print('Making session to connect to changeLog.db')
+                session.add(new_data)  # Add data to changeLog database
+                # print('data added to changelog db, please commit')
+                # session.add(new_data)
+                try:
+                    session.commit()
+                    # print('Recieved data added to changeLog.db.')
+                    currentUser = 'Colin'
+                    print('Set values/protocol changed by: ' + currentUser)
+                except:
+                    print('Error in committing data.')
+            else:
+                # The password is false
+                print('The password was incorrect')
+                # Play a gif that is about getting passwords wrong
+        except:
+            print('Data was not added because an error was thrown in /setProtocol')
+        print('I finished currentRecieve')  # The server mangager now knows that this method has finished.
+        return 'success'
+
     @app.route('/currentRecieve', methods=["POST"])
     def currentRecieve():
         '''
@@ -137,16 +133,16 @@ class Taris_SW:
         print('Recieved data at: /currentRecieve.')
         # Process a JSON. Put JSON data in a database.
         try:
-            myJSON = request.get_json(force=True) # myJSON is string not json
+            myJSON = request.get_json(force=True)  # myJSON is string not json
             print('request contained a json')
-            #myJSON.json()
+            # myJSON.json()
             realJSON = json.loads(myJSON)
             print('json loaded')
-            #attachME = str(realJSON['payload']['pH'])
-            #print('myJSON[payload][pH] is:' + attachME) # insert to test
+            # attachME = str(realJSON['payload']['pH'])
+            # print('myJSON[payload][pH] is:' + attachME) # insert to test
 
             ######### Put things into the DB ###############--#
-            mytime = time.strftime('%D %H:%M:%S') # get server time
+            mytime = time.strftime('%D %H:%M:%S')  # get server time
 
             temp = realJSON['payload']['temp']
             mypH = realJSON['payload']['pH']
@@ -157,29 +153,29 @@ class Taris_SW:
             purifier = 1
 
             new_data = bioDec(temperature=temp, pH=mypH, timeData=mytime,
-                                       NaOH=NaOH, heater=heater, inFlow=inFlow,
-                                       outFlow=outFlow, purifier=purifier)
+                              NaOH=NaOH, heater=heater, inFlow=inFlow,
+                              outFlow=outFlow, purifier=purifier)
             print('new data made')
             # mydatetime = mydatetimer(mytime)
             # new_data = Bioreactor_Data(temperature= temp,pH = mypH,timedata=mydatetime)
-            session = Taris_SW.makeBioreactorEngine()
+            session = makeBioreactorEngine()
             session.add(new_data)
-            #session.add(new_data)
+            # session.add(new_data)
             session.commit()
             print('Recieved data added to database.')
         except:
             print("currentRecieve was called but the data was not sent to the database.")
 
-        print('I finished currentRecieve') # The server mangager now knows that this method has finished.
+        print('I finished currentRecieve')  # The server mangager now knows that this method has finished.
         return 'success'
-
 
     @app.route('/setToPIREAD')
     def setToPIREAD():
-      '''
-      Get the last value in changeLog, tell the pi what to set to
-      '''
-      pass
+        '''
+        Get the last value in changeLog, tell the pi what to set to
+        '''
+        pass
+
     #############################################################################################
 
 
@@ -193,7 +189,6 @@ class Taris_SW:
         '''GETS the plot page'''
 
         return render_template('plots.html')
-
 
     #############################################################
     #   The following methods contain the route calls for the graphic display if
@@ -218,82 +213,26 @@ class Taris_SW:
     def heaterPage():
 
         return render_template('plotsHeater.html')
+
     #############################################################
 
     @app.route('/params')
     def paramsPage():
         '''GETS parameter page'''
         try:
-          lastProto = Taris_SW.getProtocol()
-          loadSetPH = lastProto.setPH
-          loadSetTemp = lastProto.setTemp
+            lastProto = getProtocol()
+            loadSetPH = lastProto.setPH
+            loadSetTemp = lastProto.setTemp
         except:
-          print('Data failed to load from DB.')
-          loadSetTemp = 'FAIL'
-          loadSetPH = 'FAIL'
-          
-        return render_template('testParams.html', setPH = loadSetPH, loadTemp = loadSetTemp )
+            print('Data failed to load from DB.')
+            loadSetTemp = 'FAIL'
+            loadSetPH = 'FAIL'
 
-##########################$$$ Database Access Methods $$$##########################
-    # These methods are database engines and access, see descriptions
-    def makeBioreactorEngine():
-        '''
-        session maker for Bioreactor.db
-        return: database session for Bioreactor.db
-        '''
-        engine = create_engine('sqlite:///Bioreactor.db')  # databaseName.db goes here
-        Base.metadata.bind = engine
-        DBSession = sessionmaker(bind=engine)
-        BioreactorDBsession = DBSession()
-        return BioreactorDBsession
-    
-    def makeChangeEngine():
-        '''
-        session maker for changeLog.db
-        return: database session for changLog.db
-        '''
-        newEngine = create_engine('sqlite:///changeLog.db')
-        logBase.metadata.bind = newEngine
-        DBSession = sessionmaker(bind=newEngine)
-        changeSession = DBSession()
-        return changeSession
-    def getProtocol():
-      '''
-      Returns the last protocol that was committed. (most recent set ph and temp here)
-      :return: current protocol in the changelog DB
-      '''
-      session = Taris_SW.makeChangeEngine()
-      # http://stackoverflow.com/questions/8551952/how-to-get-last-record
-      # last was inspired/copied from Stackoverflow user: miku, thank you miku
-      last = session.query(changeLog).order_by(changeLog.timeLog.desc()).first()
-      return last
-
-    def getValues():
-        '''
-        WARNING : Gets all values of the database
-        return: all database entries
-        '''
-        session = Taris_SW.makeBioreactorEngine()
-        allBioData = session.query(bioDec).all()
-        return allBioData
-
-    def getLast():
-        '''
-        Gets the last entry in the database.
-        return: last entry in the databsee
-        '''
-        session = Taris_SW.makeBioreactorEngine()
-        # http://stackoverflow.com/questions/8551952/how-to-get-last-record
-        # last was inspired/copied from Stackoverflow user: miku, thank you miku
-        last = session.query(bioDec).order_by(bioDec.timedata.desc()).first()
-        return last
-##########################$$$ Database Access Methods $$$##########################
-
- 
+        return render_template('testParams.html', setPH=loadSetPH, loadTemp=loadSetTemp)
 
 
-myTaris=Taris_SW()
 
+myTaris = Taris_SW()
 
 if __name__ == '__main__':
     app.run()
--- taris_sever/setupDB.py
+++ taris_sever/setupDB.py
@@ -20,14 +20,14 @@ import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
 class bioDec(Base):
     '''
-    
+    bioDec creates a database that will hold the parsed JSON files sent from the raspberry pi.
     '''
     # Create the tablename that will be used to query the columns and rows later.
     __tablename__ = 'bio_Data'
@@ -48,23 +48,88 @@ class bioDec(Base):
 
 logBase = declarative_base()
 class changeLog(logBase):
+    '''
+	changeLog creates a database that will hold the updated parameters for the bioreactor.
 	'''
-	user_Log creates a database that will hold the name of the person that edits a parameter for the bioreactor.
-	'''
+    __tablename__ = 'userInfo'
 
-	__tablename__ = 'userInfo'
+    # timeLog will take the server time when a user changes a parameter. Similar to timeData in bioDec.
+    timeLog = Column('timeLog', String, primary_key=True)
+    username = Column('username', String)
+    password = Column('password', String)
+    setPH = Column('setPH', Integer)
+    setTemp = Column('setTemp', Integer)
+    timeHold = Column('timeHold', Integer)
 
-	# timeLog will take the server time when a user changes a parameter. Similar to timeData in bioDec.
-	timeLog = Column('timeLog', String, primary_key=True)
-	username = Column('username', String)
-	password = Column('password', String)
-	setPH = Column('setPH', Integer)
-    	setTemp = Column('setTemp', Integer)
-    	timeHold = Column('timeHold', Integer)
-
-
-engine = create_engine('sqlite:///Bioreactor.db')
-Base.metadata.create_all(engine)
+# Create Databse
+bioreactorEngine = create_engine('sqlite:///Bioreactor.db')
+runBio = Base.metadata.create_all(bioreactorEngine)
 
 logEngine = create_engine('sqlite:///changeLog.db')
-logBase.metadata.create_all(logEngine)
+changeLogEngine = logBase.metadata.create_all(logEngine)
+
+
+##########################$$$ Database Access Methods $$$##########################
+# These methods will imported into TarisV1.py with setupDB.py, allowing users to connect and work with the different databases.
+
+def makeBioreactorEngine():
+    '''
+    session maker for Bioreactor.db
+    return: database session for Bioreactor.db
+    '''
+    global bioreactorEngine
+    Base.metadata.bind = bioreactorEngine
+    DBSession = sessionmaker(bind=bioreactorEngine)
+    BioreactorDBsession = DBSession()
+    return BioreactorDBsession
+
+
+def makeChangeEngine():
+    '''
+    session maker for changeLog.db
+    return: database session for changLog.db
+    '''
+    global logEngine
+    logBase.metadata.bind = logEngine
+    DBSession = sessionmaker(bind=logEngine)
+    changeSession = DBSession()
+    return changeSession
+
+
+
+#########
+#Access and Query the database for last entries for the two datbases.
+
+def getProtocol():
+    '''
+    Returns the last protocol that was committed. (most recent set ph and temp here)
+    :return: current protocol in the changelog DB
+    '''
+    session = makeChangeEngine()
+    # http://stackoverflow.com/questions/8551952/how-to-get-last-record
+    # last was inspired/copied from Stackoverflow user: miku, thank you miku
+    last = session.query(changeLog).order_by(changeLog.timeLog.desc()).first()
+    return last
+
+
+def getValues():
+    '''
+    WARNING : Gets all values of the database
+    return: all database entries
+    '''
+    valSession = makeBioreactorEngine()
+    allBioData = valSession.query(bioDec).all()
+    return allBioData
+
+
+def getLast():
+    '''
+    Gets the last entry in the database.
+    return: last entry in the databsee
+    '''
+    lastSession = makeBioreactorEngine()
+    # http://stackoverflow.com/questions/8551952/how-to-get-last-record
+    # last was inspired/copied from Stackoverflow user: miku, thank you miku
+    last = lastSession.query(bioDec).order_by(bioDec.timedata.desc()).first()
+    return last
+
